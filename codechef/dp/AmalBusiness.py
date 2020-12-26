a467200df75dabdc8a924043e502c8f2d4dd033f
Muniyasamy <vrkmuniyasamy95@gmail.com>
2020-12-26 13:26:55 +0530

    Create AmalBusiness.py

---
+++ codechef/dp/AmalBusiness.py
@@ -0,0 +1,26 @@
+#code {HW2E}
+
+import sys
+
+def maximumNonDecreasingOrder(n,arr):
+    max = int(0)
+    sum = int(0)
+    for i in range(1,n):
+      if ( arr[i-1] < arr[i] ) :
+          sum = sum + 1
+      else:
+         sum = 0
+
+      if sum >= max :
+        max = sum
+
+    return ( max + 1 ) if max > 0 else 0 
+
+
+def main():
+
+    n = int(input()) ;
+    arr = list(map(int,sys.stdin.readline().split(' ')[:n]))
+    print(maximumNonDecreasingOrder(n,arr))
+
+main()
